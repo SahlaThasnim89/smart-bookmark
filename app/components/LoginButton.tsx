aecee4a41454ab9c975b528faf4381d80012e5bc
SahlaThasnim89 <sahlathasnim2002@gmail.com>
2026-02-18 12:02:18 +0530

    fix:change origin

--- app/components/LoginButton.tsx
+++ app/components/LoginButton.tsx
@@ -13,7 +13,7 @@ export default function LoginButton() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: `${location.origin}/auth/callback`,
+          redirectTo: `${window.location.origin}/auth/callback`,
         },
       });
 
